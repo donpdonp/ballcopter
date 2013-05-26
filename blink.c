801ae00bfbcd1be39cee9062f21272683bf9ec54
Don Park <donp@donp.org>
2013-05-25 22:16:28 -0700

    uart bits

--- blink.c
+++ blink.c
@@ -5,18 +5,23 @@
 #include "driverlib/gpio.h"
 #include "driverlib/rom.h"
 #include "driverlib/sysctl.h"
+#include "utils/uartstdio.h"
  
 #define LED_RED GPIO_PIN_1
 #define LED_BLUE GPIO_PIN_2
 #define LED_GREEN GPIO_PIN_3
  
 void delayuS(int);
+void uartsetup();
  
 int main() {
   ROM_SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
   ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
   ROM_GPIOPinTypeGPIOOutput(GPIO_PORTB_BASE, LED_RED|LED_BLUE|LED_GREEN);
+
+  uartsetup();
   int speed = 1100;
+
   for (;;) {
     // set the red LED pin high, others low
     ROM_GPIOPinWrite(GPIO_PORTB_BASE, LED_RED|LED_GREEN|LED_BLUE, LED_RED);
@@ -29,5 +34,13 @@ int main() {
 void delayuS(int us) {
   // loop us times, with 3 processor cycles per loop
   ROM_SysCtlDelay( (ROM_SysCtlClockGet()/(3*1000000))*us );
+}
+
+void uartsetup() {
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+    ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+    UARTStdioInit(0);
 }
 
