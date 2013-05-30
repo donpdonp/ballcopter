c7cabd0ed7f9fd7ee5f644240fd0f94e78d81d48
Don Park <donp@donp.org>
2013-05-29 17:53:59 -0700

    uart libs

---
+++ uartstdio.d
@@ -0,0 +1,11 @@
+uartstdio.o: uartstdio.c \
+ /home/donp/code/arm/gcc-arm-none-eabi-4_6-2012q4/bin/../lib/gcc/arm-none-eabi/4.6.2/include/stdarg.h \
+ ../yuvadm-stellaris/inc/hw_ints.h ../yuvadm-stellaris/inc/hw_memmap.h \
+ ../yuvadm-stellaris/inc/hw_types.h ../yuvadm-stellaris/inc/hw_uart.h \
+ ../yuvadm-stellaris/driverlib/debug.h \
+ ../yuvadm-stellaris/driverlib/interrupt.h \
+ ../yuvadm-stellaris/driverlib/rom.h \
+ ../yuvadm-stellaris/driverlib/rom_map.h \
+ ../yuvadm-stellaris/driverlib/sysctl.h \
+ ../yuvadm-stellaris/driverlib/uart.h \
+ ../yuvadm-stellaris/utils/uartstdio.h
